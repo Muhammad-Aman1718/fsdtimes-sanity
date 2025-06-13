6d99267fae733b143d73c1eade433476eadd4617
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-13 19:50:54 +0500

    Working on categories

--- schemaTypes/category.ts
+++ schemaTypes/category.ts
@@ -17,5 +17,54 @@ export const category = defineType({
       type: 'slug',
       options: {source: 'name'},
     }),
+
+    // Continent dropdown
+    defineField({
+      name: 'continent',
+      title: 'Continent',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Asia', value: 'asia'},
+          {title: 'Europe', value: 'europe'},
+          {title: 'Africa', value: 'africa'},
+          {title: 'North America', value: 'north-america'},
+          {title: 'South America', value: 'south-america'},
+          {title: 'Australia', value: 'australia'},
+          {title: 'Antarctica', value: 'antarctica'},
+        ],
+      },
+    }),
+
+    // defineField({
+    //   name: 'slug',
+    //   title: 'Continent Slug',
+    //   type: 'slug',
+    //   options: {source: 'continent'},
+    // }),
+
+    // Country field
+    defineField({
+      name: 'country',
+      title: 'Country',
+      type: 'string',
+    }),
+
+
+
+    // City field
+    defineField({
+      name: 'city',
+      title: 'City',
+      type: 'string',
+    }),
+
+    // Categories (array of strings)
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
   ],
 })
